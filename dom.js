2c7ecc0b13be2b8d630a432496a8769e9ebf7e8f
Jatin Rawat <jatinrawat27official@gmail.com>
2024-05-18 00:00:53 +0530

    added Html practice questions 🚀

---
+++ dom.js
@@ -0,0 +1,15 @@
+
+
+// lets select it
+
+// const para = document.querySelector('.para');
+
+// // lets change the text
+// para.textContent = "Hello this is jatin 😊";
+
+// console.log(para);
+
+// const para = document.getElementById('para_one');
+
+// // lets update the text
+// para.textContent = "Hello this is jatin";
---
+++ js/script.js
@@ -0,0 +1,57 @@
+// DOM Stands For Document Object Model
+
+// When The Project Loads , File Loads , Thw Web Browser Loads it into a tree like structure 
+// Get Element by ID to grab elements  from the document object model.
+// const def_term = document.getElementById('def_term');
+// console.log(def_term);
+// console.log(def_term.innerText);
+// console.log(def_term.innerHTML);
+// console.log(def_term.textContent);
+
+// def_term.innerText = "This is manipulation by DOM!";
+// def_term.style.color ="red";
+// def_term.style.backgroundColor="red";
+// def.term.innerText
+//offers methods and properties for selecting from the list.
+// ID`s are unique to HTML element
+
+
+// const def_term = document.getElementById('def_term');
+// const pic = document.getElementsByClassName('Top_header')
+// const HeaderAll = document.getElementsByClassName('Top_header')
+// console.log(pic)
+// console.log(pic[0])
+// console.log(pic.length);
+// console.log(def_term.length);
+
+
+// const HeaderAll = document.getElementsByClassName('Top_header')
+// console.log(HeaderAll);
+// console.log(HeaderAll[0]);
+// console.log(HeaderAll[1]);
+// console.log(HeaderAll[2]);
+
+// console.log(HeaderAll.length);
+
+// for (let x = 0 ; x < HeaderAll.length;x++) {
+// HeaderAll[x].style.backgroundColor = 'Yellow';
+
+// }
+
+
+// const HeaderTag = document.getElementsByTagName('header')
+// const ListTag = document.getElementsByTagName('ol')
+// console.log(HeaderTag , ListTag);
+
+
+// console.log(3 == '3');
+
+getName();
+console.log(x);
+let x =7;
+function getName(){
+    console.log("NJ");
+}
+
+console.log(x);
+
